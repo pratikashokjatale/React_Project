35420a89250513df1ba97f4e7031f5595eabd58e
pratikjatale <pjatale8@gmail.com>
2025-09-08 00:40:15 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-// ...existing code...
-import viteLogo from './assets/vite.svg' // <-- Use relative path
-// ...existing code...
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
